3a5f701bc3d1d9e334062b4c745ff6ec6bcd3c91
n-speech <dafna5566@gmail.com>
2025-05-13 21:00:37 +0300

    Delete js/french.js

--- js/french.js
+++
@@ -1,7 +0,0 @@
-// Запрос открытых уроков с сервера
-async function getUnlockedLessons(studentName) {
-    const response = await fetch(`https://твой-домен.onrender.com/access?student=${encodeURIComponent(studentName)}`);
-    const data = await response.json();
-    return data.unlocked || [];
-  }
-  
