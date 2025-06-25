4bc2211bcde4cb11a8303f19aaa770f4b5015d6d
n-speech <dafna5566@gmail.com>
2025-06-25 21:58:00 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
-// Запрос открытых уроков с сервера
+/* Запрос открытых уроков с сервера
 async function getUnlockedLessons(studentName) {
     const response = await fetch(`https://твой-домен.onrender.com/access?student=${encodeURIComponent(studentName)}`);
     const data = await response.json();
     return data.unlocked || [];
-  }
+  }*/
   
   // Добавляем логику для бургер-меню
 
